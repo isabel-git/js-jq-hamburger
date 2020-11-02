026528a141801d66df072572348ccee54cbbf9f8
Isabel <isabel.gogov1@libero.it>
2020-11-02 15:57:09 +0100

    completato

---
+++ main.js
@@ -0,0 +1,22 @@
+// mostrare / nascondere il menu principale
+
+var aperto = false;
+
+$('.header-right > a').click(function() {
+  if(aperto === false){
+    // $('.hamburger-menu').show(); // apri il menu hamburger al click del ham
+    $('.hamburger-menu').addClass( "active" ); // aggiunge la classe active al menu ham
+    aperto = true;
+    console.log(aperto);
+  }
+});
+
+
+$('.close').click(function() {
+  if(aperto === true){
+    // $('.hamburger-menu').hide(); // chiude il menu hamburger al click di .close
+    $('.hamburger-menu').removeClass( "active" ); // toglie la classe active
+    aperto = false;
+    console.log(aperto);
+  }
+});
